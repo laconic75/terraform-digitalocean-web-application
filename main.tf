aa3ef17a141fb3f2a376ca5c4956bed25af3b6f3
Joel Robinson <laconic75@gmail.com>
2020-12-29 16:32:35 -0600

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 1.22.2"
+    }
+  }
+}
+
+resource "digitalocean_droplet" "application" {
+  image              = var.image_name
+  name               = var.droplet_name
+  region             = var.region
+  size               = var.droplet_size
+  private_networking = var.private_networking
+  ssh_keys = var.ssh_keys
+    
+}
+
+resource "digitalocean_firewall" "standard_webserver" {
+  name = "web-server-ssh-restricted-icmp-blocked"
+  droplet_ids = [digitalocean_droplet.application.id]
+
+  inbound_rule { 
+    protocol = "tcp"
+    port_range = "22"
+    source_addresses = var.ssh_source_addresses
+  }
+
+  inbound_rule { 
+    protocol = "tcp"
+    port_range = "80"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule { 
+    protocol = "tcp"
+    port_range = "443"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol = "udp"
+    port_range = "all"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol = "tcp"
+    port_range = "all"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol = "icmp"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "image_name" {
+  type = string
+}
+
+variable "region" {
+ type = string
+ default = "nyc1"
+}
+
+variable "droplet_name" {
+  type = string
+  default = "application"
+}
+
+variable "droplet_size" {
+  type = string
+  default = "s-1vcpu-1gb"
+}
+
+variable "private_networking" {
+  type = bool
+  default = true
+}
+
+variable "ssh_keys" {
+  type = list(string)
+}
+
+variable "ssh_source_addresses" {
+  type = list(string)
+} 
